465075ccccff1681a9a6589a01e559a46b4d3080
maria <mpramos78915400@gmail.com>
2024-06-11 20:10:22 -0400

    modulo3 lista de contactos

---
+++ modulo3/lab1.js
@@ -0,0 +1,26 @@
+let contacts = [
+    {
+    name: "Maxwell Wright",
+    phone: "(0191) 719 6495",
+    email: "egestas@nonummyac.co.uk"
+    },
+    {
+    name: "Raja Villarreal",
+    phone: "0866 398 2895",
+    email: "posuere@sed.com"
+    },
+    {
+    name: "Helen Richards",
+    phone: "0800 1111",
+    email: "libero@vallis.edu"
+    }
+];
+    
+    // escribe tu código aquí
+    
+    
+    let last = contacts.length - 1;
+
+    
+    console.log(`${contacts[0].name} / ${contacts[0].phone} / ${contacts[0].email}`);
+    console.log(`${contacts[last].name} / ${contacts[last].phone} / ${contacts[last].email}`);
