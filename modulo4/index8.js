c42dce4dbc28d7478efa58c0d5ed725a9c119deb
maria <mpramos78915400@gmail.com>
2024-06-11 21:53:57 -0400

    modulo4 octavo ejercicio

---
+++ modulo4/index8.js
@@ -0,0 +1,13 @@
+// let price= 100
+// let shippingCost
+
+// if (price>40) {
+//     shippingCost=0
+// } else {
+//     shippingCost=5
+// }
+// console.log(`price ${price} , shipping = ${shippingCost}`);
+
+let price=20
+let shippingCost = price > 50 ? 0:5
+console.log(shippingCost);
